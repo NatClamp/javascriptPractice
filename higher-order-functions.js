069415f8730908663e6af20f8bceafb5f09357c4
NatClamp <nat.clamp@gmail.com>
2018-11-10 19:37:17 +0000

    practicing with higher order functions

---
+++ higher-order-functions.js
@@ -0,0 +1,53 @@
+const hof = {};
+
+hof.identity = function(value) {
+  return value;
+};
+
+hof.identityf = function() {};
+
+hof.add = function() {};
+
+hof.sub = function() {};
+
+hof.mul = function() {};
+
+hof.addf = function() {};
+
+hof.liftf = function() {};
+
+hof.curry = function() {};
+
+hof.inc = function() {};
+
+hof.twice = function() {};
+
+hof.composeu = function() {};
+
+hof.composeb = function() {};
+
+hof.limit = function() {};
+
+hof.from = function() {};
+
+hof.to = function() {};
+
+hof.fromTo = function() {};
+
+hof.element = function() {};
+
+hof.collect = function() {};
+
+hof.filter = function() {};
+
+hof.concat = function() {};
+
+hof.gensymf = function() {};
+
+hof.gensymff = function() {};
+
+hof.counter = function() {};
+
+hof.revokable = function() {};
+
+module.exports = hof;
